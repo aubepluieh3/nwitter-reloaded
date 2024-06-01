2c3f3f78ac9ad5d37f6c90f598e5d21de70c80a6
aubepluieh3 <aubepluieh3@naver.com>
2024-06-01 15:47:34 +0900

    Setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyDnaERcUhJUgoBoh61RY4egGXEiA8GUTlw",
   authDomain: "nwitter-reloaded-a943e.firebaseapp.com",
@@ -9,3 +10,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
